83fa698f6427ded97d319890a8efde7f18fe3828
john culnan <jmculnan@email.arizona.edu>
2023-01-09 13:06:59 -0800

    updated audio file splitting to run over individual speaker files

--- utils/split_audio_files.py
+++ utils/split_audio_files.py
@@ -57,8 +57,10 @@ def extract_portions_of_mp4_or_wav(
 def split_wav_into_utterances(path_to_sound_file, sound_file, wav_level_df):
     """
     Take a series of wav files and split into a group of utterance-level wav files
-    :param path_to_sound_files:
-    :param df_with_timestamps: A dataframe including timestamps and utterance IDs
+    Must use a separate wav file for each speaker in the group
+    :param path_to_sound_file: the path to the sound file
+    :param sound_file: the wav file to be split
+    :param wav_level_df: A dataframe including timestamps and utterance IDs
     :return:
     """
     wav_level_df.dropna(subset=["start_timestamp", "end_timestamp"], inplace=True)
@@ -66,8 +68,10 @@ def split_wav_into_utterances(path_to_sound_file, sound_file, wav_level_df):
         # add 250ms to start and end timestamps
         # to ensure we don't have any completely empty items
         # due to the timestamp rounding Google uses
-        wav_level_df["start_timestamp"] = wav_level_df["start_timestamp"].apply(lambda x: x + .250)
-
+        wav_level_df["start_timestamp"] = wav_level_df["start_timestamp"].apply(lambda x: convert_timestring_to_seconds(x))
+        wav_level_df["start_timestamp"] = wav_level_df["start_timestamp"] - .250
+        wav_level_df["end_timestamp"] = wav_level_df["end_timestamp"].apply(lambda x: convert_timestring_to_seconds(x))
+        wav_level_df["end_timestamp"] = wav_level_df["end_timestamp"] + .250
 
         row_names = wav_level_df["message_id"].tolist()
         start_times = wav_level_df["start_timestamp"].tolist()
@@ -83,14 +87,22 @@ def split_wav_into_utterances(path_to_sound_file, sound_file, wav_level_df):
 
 
 def split_wavs_into_utterances(path_to_sound_files, df_with_timestamps):
+    # split multiple wav files into single utterance files
+    # get the set of all trials used here
     all_trials = set(df_with_timestamps["trial_id"].tolist())
 
+    # set the path to the sound files
     filespath = Path(path_to_sound_files)
+    # iterate over files in the sound file path to find wav files
     for item in filespath.iterdir():
         if item.suffix == ".wav":
+            # get the trial and speaker for this file
             wav_trial = str(item.name).split("/")[-1].split("_")[2].split("-")[1]
+            speaker = str(item.name).split("/")[-1].split("_")[4].split("-")[1]
+            # if it is a trial of interest, split it into utterances
             if wav_trial in all_trials:
                 short_df = df_with_timestamps[df_with_timestamps["trial_id"] == wav_trial]
+                short_df = short_df[short_df["participant"] == speaker]
                 split_wav_into_utterances(item.parents[0], item.name, short_df)
 
 
@@ -106,11 +118,9 @@ def convert_timestring_to_seconds(timestring):
     return float(time_sep[0]) * 60 + float(time_sep[1])
 
 
-
-
 if __name__ == "__main__":
-    base_path = "/media/jculnan/backup/jculnan/datasets/asist_data2"
-    wav_path = f"{base_path}/audio"
-    df = pd.read_csv(f"{base_path}/overall_sent-emo.csv")
+    base_path = "/media/jculnan/One Touch/jculnan/datasets/MultiCAT"
+    wav_path = f"{base_path}/individual_speaker_audio"
+    df = pd.read_csv(f"{base_path}/processed_dataset.csv")
 
     split_wavs_into_utterances(wav_path, df)
